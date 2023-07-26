3a3e7cacfa40410e1ae4b5fda12a7a20994822ab
shane <shane2907gomes@gmail.com>
2023-07-26 10:25:34 +0530

    solved if and func tions and quiz1

--- quiz1.rs
+++ quiz1.rs
@@ -13,7 +13,15 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(x: u32) -> u32 {
+
+    if x > 40 {
+        x
+    } else {
+        x * 2
+    }
+
+}
 
 // Don't modify this function!
 #[test]
