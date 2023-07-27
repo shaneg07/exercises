fea61f0c46b95da8174e2dea6732cba869e11439
shane <shane2907gomes@gmail.com>
2023-07-27 22:21:50 +0530

    hashmaps, modules, quiz2 solved

--- hashmaps/hashmaps2.rs
+++ hashmaps/hashmaps2.rs
@@ -13,7 +13,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -36,9 +35,8 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the basket.
-        // Note that you are not allowed to put any type of fruit that's already
-        // present!
+        basket.insert(Fruit::Banana, 2);
+        basket.insert(Fruit::Pineapple, 2);
     }
 }
 
--- quiz2.rs
+++ quiz2.rs
@@ -18,7 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -30,20 +29,34 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(x) => output.push(append_bar(string, *x)),
+            }
         }
         output
+    }
+    fn append_bar(s: &String, x: usize) -> String {
+        let mut s1 = s.to_string();
+        for _ in 0..x {
+            s1.push_str("bar")
+        }
+
+        s1
+
     }
 }
 
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
