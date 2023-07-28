ae44482c13a74732acec240e3dfe24a66c298a33
shane <shane2907gomes@gmail.com>
2023-07-28 11:13:07 +0530

    errors, generics, options, traits solved.

--- generics/generics2.rs
+++ generics/generics2.rs
@@ -3,14 +3,12 @@
 
 // Execute `rustlings hint generics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-struct Wrapper {
-    value: u32,
+struct Wrapper<R> {
+    value: R,
 }
 
-impl Wrapper {
-    pub fn new(value: u32) -> Self {
+impl<R> Wrapper<R>{
+    pub fn new(value: R) -> Self {
         Wrapper { value }
     }
 }
